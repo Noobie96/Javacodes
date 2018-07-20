9bab8223734c56d03e7845d8820be8f379a2b609
Noobie96 <41187593+Noobie96@users.noreply.github.com>
2018-07-20 07:05:03 -0700

    Add files via upload

---
+++ Al.java
@@ -0,0 +1,17 @@
+package collections;
+import java.util.ArrayList;
+//import java.io.Scanner;
+public class Al{
+	public static void main(String args[]) {
+//Scanner sc = new Scanner();		
+ArrayList<Object> arr = new ArrayList<>();
+//ArrayList arr1 = new ArrayList<>();
+arr.add(5);
+arr.add(2);
+System.out.println("op:"+arr.get(1));
+//for(int i=0;i<=arr.size();i++) {
+
+	
+	}
+}
+//}
